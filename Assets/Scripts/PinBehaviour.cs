3f82ea05b0af565fec05bd6e6503408ada536d1a
oussamabonnor1 <oussamabonnor@gmail.com>
2019-12-27 02:31:29 +0100

    adding Needle

---
+++ Assets/Scripts/PinBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinBehaviour : MonoBehaviour
+{
+    public int speed;
+    public Rigidbody2D rb;
+    public bool shoot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(shoot) rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.tag == "Rotator")
+        {
+            shoot = false;
+            transform.parent = other.transform;
+        }
+    }
+}
---
+++ Assets/Scripts/PinSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinSpawner : MonoBehaviour
+{
+    public GameObject pin;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) Instantiate(pin, transform.position, transform.rotation);
+    }
+}
