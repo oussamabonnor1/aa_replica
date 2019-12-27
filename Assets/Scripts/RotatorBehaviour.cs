f019c54128a49233ec0ee3b40b25e4ac81a8dbe6
oussamabonnor1 <oussamabonnor@gmail.com>
2019-12-27 01:00:29 +0100

    adding Needle

---
+++ Assets/Scripts/RotatorBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorBehaviour : MonoBehaviour
+{
+    public int rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+}
